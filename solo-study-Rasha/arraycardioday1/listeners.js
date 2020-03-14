fa81f3851fd3dbcd5e220e54b912e46b0c20124c
Rasha ali <rashaali84@gmail.com>
2020-03-14 22:01:02 +0100

    solo learning arrays Rasha

---
+++ solo-study-Rasha/arraycardioday1/listeners.js
@@ -0,0 +1,11 @@
+debugger;
+document
+    .getElementById("btn_Filter")
+    .addEventListener("click", handlerForFilter);
+document
+    .getElementById("btn_Map")
+    .addEventListener("click", handlerForMap);
+document
+    .getElementById("btn_Sort")
+    .addEventListener("click", sortAlphabatically);
+
